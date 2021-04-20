91a16b77c01285cd0a758071dc50fbc6043baf26
AmandaVieski <amanda.b.vieski@gmail.com>
2021-04-20 18:30:57 -0300

    fim da parte 5 do tutorial de questões e respostas

--- mysite_project/polls/models.py
+++ mysite_project/polls/models.py
@@ -1,3 +1,34 @@
+# Create your models here.
+import datetime
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+"""
+class BaseModel(models.Model):
+    objects = models.Manager()
+    class Meta:
+        abstract = True
+"""
+
+#class Question(BaseModel):
+class Question(models.Model):
+    #objects = models.Manager()
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+#class Choice(BaseModel):
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+       return self.choice_text
